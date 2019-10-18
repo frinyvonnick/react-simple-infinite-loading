4913a391f8812e62e28de25ea9d99eb5da4faa41
Jimmy Kasprzak <jimmykasprzak@gmail.com>
2019-10-18 19:41:33 +0200

    :sparkles: Improve example functionality and ui (#25)
    * :sparkles: create infinite loading list in example
    
    * :lipstick: improve example ui

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import InfiniteLoading from 'react-simple-infinite-loading'
 
 export default function App() {
-  const items = [...Array(100)].map((_, index) => index)
+  const [items, setItems] = useState([...Array(100)].map((_, index) => index))
+
+  const loadMoreItems = () => {
+    const newItems = [...Array(100)].map((_, index) => items.length + index)
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        setItems([...items, ...newItems])
+        resolve()
+      }, 300)
+     })
+  }
 
   return (
-    <div style={{ width: 300, height: 300 }}>
+    <div className="app">
+      <h1>React simple infinite loading example</h1>
+
+      <h2>Start scrolling here :</h2>
       <InfiniteLoading
-        hasMoreItems={false}
+        hasMoreItems
         itemHeight={40}
-        loadMoreItems={() => {}}
+        loadMoreItems={loadMoreItems}
       >
-        {items.map(item => <div key={item}>{item}</div>)}
+        {items.map(item => <div className="item" key={item}>{item}</div>)}
       </InfiniteLoading>
     </div>
   )
